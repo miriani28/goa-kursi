39cfe71b9925225cd0dcdc6dfe8dc475286ad0c0
miriani28 <miriankobalia@gmail.com>
2024-03-27 20:43:53 +0400

    commit

---
+++ python/day-1/data.py
@@ -0,0 +1,5 @@
+name = "mirian"
+age = 9
+height = 143
+
+print("my name is", name, "i am", age, "years old", "my height is", height, "cm")
---
+++ python/day-7/lesson 7.py
@@ -0,0 +1,3 @@
+favorite_candy="twix"
+age=9
+print(favorite_candy+str(age))
